0b8f0d9b1be2886d84e508f223e1faf6d9239bd3
Misael Morgado <112130147+MisaelBM@users.noreply.github.com>
2024-08-11 23:18:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+//Funcao que inicia o jogo
+function StartGame() {
+    //Ordem dos parametros recebidos: typeQuiz, visorQuest, data1, data2, data3, realRes
+    let parametersQuest = EquationQuest();
+    document.getElementById("spanQuest").innerHTML = `<div class="quiz-multiple-choice" id="multipleChoice">
+            <div class="multiple-choice-visor-content">
+                <span class="multiple-choice-visor" id="multipleChoiceVisor">${parametersQuest[0]}</span>
+            </div>
+            <div class="multiple-choice-buttons-content">
+                <button class="multiple-choice-buttons" data-buttonData="${parametersQuest[1]}">${parametersQuest[1]}</button>
+                <button class="multiple-choice-buttons" data-buttonData="${parametersQuest[2]}">${parametersQuest[2]}</button>
+                <button class="multiple-choice-buttons" data-buttonData="${parametersQuest[3]}">${parametersQuest[3]}</button>
+            </div>
+    </div>`;
+    document.querySelectorAll(".multiple-choice-buttons").forEach(e => {
+        e.addEventListener('click', function() {ConfirmRes(e)});
+    });
+    function ConfirmRes(event) {
+        document.querySelectorAll(".multiple-choice-buttons").forEach(el => {
+            el.removeEventListener('click', function() {ConfirmRes(e)});
+        });
+        return console.log(parseInt(event.getAttribute("data-buttonData")) == parametersQuest[4] ? true : false);
+    };
+};
+function EquationQuest() {
+    const resEquation = parseInt(Math.random() * (20 - 1) + 1);
+    let typeOperation = parseInt(Math.random() * 2);
+    let secondElementEquation = parseInt(Math.random() * ((resEquation + (resEquation / 2)) - (resEquation / 2)) + (resEquation / 2));
+    while (resEquation - secondElementEquation == 0 || resEquation + secondElementEquation == 0) {
+        secondElementEquation = parseInt(Math.random() * ((resEquation + (resEquation / 2)) - (resEquation / 2)) + (resEquation / 2));
+    };
+    let XOfEquation = parseInt(Math.random() * (11 - 1) + 1);
+    while (typeOperation ? ((resEquation - secondElementEquation) % XOfEquation != 0) || (resEquation - secondElementEquation) % 1 != 0 : ((resEquation + secondElementEquation) % XOfEquation != 0) || (resEquation + secondElementEquation) % 1 != 0) {
+        XOfEquation = parseInt(Math.random() * (11 - 1) + 1);
+    };
+    let possibleRes = [XOfEquation];
+    for (let i = 0; i < 2; i++) {
+        let fakeRes = parseInt(Math.random() * (11 - 1) + 1);
+        while (possibleRes.indexOf(fakeRes) > -1) {
+            fakeRes = parseInt(Math.random() * (11 - 1) + 1);
+        };
+        possibleRes.push(fakeRes);
+    };
+    for (let i = possibleRes.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [possibleRes[i], possibleRes[j]] = [possibleRes[j], possibleRes[i]];
+    };
+    return [`${typeOperation ? (resEquation - secondElementEquation) / XOfEquation : (resEquation + secondElementEquation) / XOfEquation}X ${typeOperation ? "+" : "-"} ${secondElementEquation} = ${resEquation}`, possibleRes[0], possibleRes[1], possibleRes[2], XOfEquation];
+};
+StartGame();
